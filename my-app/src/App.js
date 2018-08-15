7ed4b1541b298b0cee5537ae26d8111acb08e387
egorzay <eleven@gscw.ru>
2018-08-15 13:54:39 +0800

    div.App wrapper added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,19 +4,21 @@ import { List } from 'semantic-ui-react';
 import '../src/semantic/dist/semantic.css';
 
 const ListExampleOrderedSimple = () => (
-  <List as='ol'>
-    <List.Item as='li'>Signing Up</List.Item>
-    <List.Item as='li'>User Benefits</List.Item>
-    <List.Item as='li'>
-      User Types
-      <List.List as='ol'>
-        <List.Item as='li'>Admin</List.Item>
-        <List.Item as='li'>Power User</List.Item>
-        <List.Item as='li'>Regular User</List.Item>
-      </List.List>
-    </List.Item>
-    <List.Item as='li'>Deleting Your Account</List.Item>
-  </List>
+  <div className="App">
+    <List as='ol'>
+      <List.Item as='li'>Signing Up</List.Item>
+      <List.Item as='li'>User Benefits</List.Item>
+      <List.Item as='li'>
+        User Types
+        <List.List as='ol'>
+          <List.Item as='li'>Admin</List.Item>
+          <List.Item as='li'>Power User</List.Item>
+          <List.Item as='li'>Regular User</List.Item>
+        </List.List>
+      </List.Item>
+      <List.Item as='li'>Deleting Your Account</List.Item>
+    </List>
+  </div>
 )
 
 export default ListExampleOrderedSimple
