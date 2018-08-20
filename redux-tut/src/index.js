ce2b704a639ef45c6b2e48ab84c0ecad52a9a009
egorzay <eleven@gscw.ru>
2018-08-20 22:29:35 +0800

    redux-todolist-tutorial added;

--- redux-tut/src/index.js
+++ redux-tut/src/index.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
 
 const store = createStore(rootReducer)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
+registerServiceWorker()
