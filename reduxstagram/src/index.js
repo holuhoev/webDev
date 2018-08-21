04ca083e02e9a97aa0b049bc0d33ef9130d8f8ab
egorzay <eleven@gscw.ru>
2018-08-21 13:20:37 +0800

    styl to css manual convert;

--- reduxstagram/src/index.js
+++ reduxstagram/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/style.styl';
+import './styles/style.css';
 import Main from './components/Main';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <BrowserRouter>
+        <Main />
+    </BrowserRouter>,
+    document.getElementById('root')
+)
+registerServiceWorker()
