57b23428c373ea972b1f0d19e3aa9eeb5715fad0
egorzay <eleven@gscw.ru>
2018-08-21 12:45:20 +0800

    deleted;

--- abc/src/page-default.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PageDefault from './page-default';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PageDefault />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
